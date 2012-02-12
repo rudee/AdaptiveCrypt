51f6bf2b42690d4c43b7948f893774ebf4c780d5
rudee <rudyleehs@gmail.com>
2012-02-12 12:43:35 +0800

    Added IHashingService

---
+++ src/AdaptiveCrypt/IHashingService.cs
@@ -0,0 +1,13 @@
+﻿namespace AdaptiveCrypt
+{
+    public interface IHashingService
+    {
+        int SaltLength { get; }
+
+        int Workfactor { get; }
+
+        string Hash(string str,
+                    string salt,
+                    int    workFactor);
+    }
+}
