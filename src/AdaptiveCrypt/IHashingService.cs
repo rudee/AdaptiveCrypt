ec01568b9280ed81edf563050be9ab25d662587d
rudee <rudyleehs@gmail.com>
2012-02-27 23:15:06 +0800

    Added documentation

--- src/AdaptiveCrypt/IHashingService.cs
+++ src/AdaptiveCrypt/IHashingService.cs
@@ -2,10 +2,23 @@
 {
     public interface IHashingService
     {
+        /// <summary>
+        /// Gets the length in characters for salts to be used by this hashing service.
+        /// </summary>
         int SaltLength { get; }
 
+        /// <summary>
+        /// Gets the work factor to be used by this hashing service.
+        /// </summary>
         int Workfactor { get; }
 
+        /// <summary>
+        /// Creates a cryptographic hash.
+        /// </summary>
+        /// <param name="str">The value to hash</param>
+        /// <param name="salt">The salt to use to create the cryptographic hash</param>
+        /// <param name="workFactor">The work factor to use to create the cryptographic hash</param>
+        /// <returns></returns>
         string Hash(string str,
                     string salt,
                     int    workFactor);
